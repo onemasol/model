65c127a093237f71c53d67c2a82b37a16d898744
Kim-Yiji <yiji_kim@yonsei.ac.kr>
2025-06-24 16:36:44 +0000

    fix: envionment fixed. please follow readme instructions

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-1. chmod +x setup.sh
-2. scp -r /Users/yiji/.cache/huggingface root@vessl-onemasol:/root/.cache/ : 약 1분
+1. cd model
+2. chmod +x setup.sh
 3. ./setup.sh
 3. ollama serve & ollama pull exaone3.5:7.8b : 약 10분 or 
     scp -r ~/.ollama/models root@vessl-onemasol:/root/.ollama/ : 3번 대신 선택 가능 약 17분
 5. ./run.sh <실행할 Python 스크립트 경로> [추가 인자]
+
+-  scp -r /Users/yiji/.cache/huggingface root@vessl-onemasol:/root/.cache/ : 약 1분 : 생략하고 추후 진행
--- setup.sh
+++ setup.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 # unzip, curl 설치
-apt update && apt install -y unzip curl
+apt update && apt install -y unzip curl software-properties-common
+
+# PPA 추가 (에러 무시)
+add-apt-repository ppa:deadsnakes/ppa -y || true
+apt update
+
+# python3.13 설치 (3.13.5 버전 설치됨)
+apt install -y python3.13 python3.13-venv python3.13-dev
+
+# pip 설치
+curl -sS https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+/usr/bin/python3.13 get-pip.py
 
 # 패키지 설치
-pip install -r /root/model/requirements.txt --root-user-action=ignore
+/usr/bin/python3.13 -m pip install -r /root/model/requirements_2.txt --root-user-action=ignore
 
 # Ollama 설치 (존재 여부 체크)
 if ! which ollama > /dev/null; then
